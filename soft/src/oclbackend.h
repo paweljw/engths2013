d8dbb618dc78bedfe6104ff7bfb88099132a0272
Paweł J. Wal <pjw@paweljw.eu>
2013-12-26 14:05:43 +0100

    Enumeracja device'ów

--- soft/src/oclbackend.h
+++ soft/src/oclbackend.h
@@ -38,6 +38,52 @@ namespace PJWFront
 			/// This OpenCL backend will only hold a single program
 			cl_program program;
 
+			cl_int enumerateDevices()
+			{
+				clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, NULL, &deviceCount);
+				devices = (cl_device_id*) malloc(sizeof(cl_device_id) * deviceCount);
+				clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, deviceCount, devices, NULL);
+
+				// for each device print critical attributes
+				for (j = 0; j < deviceCount; j++) {
+
+					// print device name
+					clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, NULL, &valueSize);
+					value = (char*) malloc(valueSize);
+					clGetDeviceInfo(devices[j], CL_DEVICE_NAME, valueSize, value, NULL);
+					printf("%d. Device: %s\n", j+1, value);
+					free(value);
+
+					// print hardware device version
+					clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, 0, NULL, &valueSize);
+					value = (char*) malloc(valueSize);
+					clGetDeviceInfo(devices[j], CL_DEVICE_VERSION, valueSize, value, NULL);
+					printf(" %d.%d Hardware version: %s\n", j+1, 1, value);
+					free(value);
+
+					// print software driver version
+					clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, 0, NULL, &valueSize);
+					value = (char*) malloc(valueSize);
+					clGetDeviceInfo(devices[j], CL_DRIVER_VERSION, valueSize, value, NULL);
+					printf(" %d.%d Software version: %s\n", j+1, 2, value);
+					free(value);
+
+					// print c version supported by compiler for device
+					clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, 0, NULL, &valueSize);
+					value = (char*) malloc(valueSize);
+					clGetDeviceInfo(devices[j], CL_DEVICE_OPENCL_C_VERSION, valueSize, value, NULL);
+					printf(" %d.%d OpenCL C version: %s\n", j+1, 3, value);
+					free(value);
+
+					// print parallel compute units
+					clGetDeviceInfo(devices[j], CL_DEVICE_MAX_COMPUTE_UNITS,
+							sizeof(maxComputeUnits), &maxComputeUnits, NULL);
+					printf(" %d.%d Parallel compute units: %d\n", j+1, 4, maxComputeUnits);
+
+				}
+				free(devices);
+			}
+			
 			/// A helper function to get and select an OpenCL platform
 			cl_int oclGetPlatformID(cl_platform_id* clSelectedPlatformID)
 			{
@@ -217,6 +263,8 @@ namespace PJWFront
 				   exit(error);
 				}
 
+				enumerateDevices();
+				
 				// Read what the device is
 				size_t valueSize;
 				char* value;
